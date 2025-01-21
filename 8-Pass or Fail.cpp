6b02d058a270b898b2777410356105b3f0fb9001
abdalrahmanDifallh <abddf98@gmail.com>
2025-01-21 13:59:30 +0300

    8-

---
+++ 8-Pass or Fail.cpp
@@ -0,0 +1,23 @@
+
+//Pass or fail
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int mark;
+   cout<<"Enter the mark : ";
+   cin>>mark;
+
+   if(mark>=50)
+    cout<<"Pass";
+   else cout<<"Fail";
+
+
+    return 0;
+}
+
+
+
+
