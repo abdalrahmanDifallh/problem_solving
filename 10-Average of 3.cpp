753ba2a2365ff78a475ea89477f8b9f931307145
abdalrahmanDifallh <abddf98@gmail.com>
2025-01-21 14:22:37 +0300

    10-Average of 3

---
+++ 10-Average of 3.cpp
@@ -0,0 +1,18 @@
+
+
+//Average of 3
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int n1,n2,n3;
+   cout<<"Enter the numbers "<<endl;
+   cin>>n1>>n2>>n3;
+
+   cout<<"Average of three numbers is "<<(n1+n2+n3)/3;
+
+
+    return 0;
+}
