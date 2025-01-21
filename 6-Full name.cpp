afb893820be478acf7327f6e370a840804a432be
abdalrahmanDifallh <abddf98@gmail.com>
2025-01-21 13:47:11 +0300

    6-full name

---
+++ 6-Full name.cpp
@@ -0,0 +1,22 @@
+//full name
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    string firstName,secondName;
+    cout<<"Enter the first name : ";
+    cin>>firstName;
+    cout<<"Enter the second name : ";
+    cin>>secondName;
+
+    cout<<"full name is "<<firstName<<" "<<secondName;
+    return 0;
+}
+
+
+
+
+
