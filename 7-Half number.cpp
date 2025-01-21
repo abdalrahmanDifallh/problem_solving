71b73599579e15db51e9f6680c70e3aa56bd4e38
abdalrahmanDifallh <abddf98@gmail.com>
2025-01-21 13:54:24 +0300

    7-Half number

---
+++ 7-Half number.cpp
@@ -0,0 +1,19 @@
+
+//Half number
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+    cout<<"Enter the number : ";
+    cin>>number;
+
+    cout<<"Half of the "<<number<<" is "<<number/2;
+    return 0;
+}
+
+
+
+
