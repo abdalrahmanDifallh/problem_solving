4822bc59cce926736177ff9cc9e0e53439c697f1
abdalrahmanDifallh <abddf98@gmail.com>
2025-01-21 07:07:59 +0300

    creat program for print naem

---
+++ 1- print_name.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+void printName(string name)
+{
+    cout<<"\n The name is : "<<name<<endl;
+}
+int main()
+{
+    printName("Abd");
+    return 0;
+}
---
+++ 1.cpp
@@ -0,0 +1,18 @@
+//print name
+#include <iostream>
+
+using namespace std;
+
+void printName(string name)
+{
+    cout<<"\nThe name is : "<<name;
+}
+int main()
+{
+
+    printName("Abd alrahman");
+    return 0;
+}
+
+
+
