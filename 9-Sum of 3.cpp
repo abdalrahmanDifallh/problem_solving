e1d27fb90a7fc527834a2acdb69275086ec65bd2
abdalrahmanDifallh <abddf98@gmail.com>
2025-01-21 14:09:07 +0300

    9-Sum of 3

---
+++ 9-Sum of 3.cpp
@@ -0,0 +1,17 @@
+
+//sum of 3
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int n1,n2,n3;
+   cout<<"Enter the numbers "<<endl;
+   cin>>n1>>n2>>n3;
+
+   cout<<"sum of three numbers is "<<n1+n2+n3;
+
+
+    return 0;
+}
