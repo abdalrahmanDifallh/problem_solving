e728ee566e83b5e0f51911005fd53e070521d44f
abdalrahmanDifallh <abddf98@gmail.com>
2025-01-21 17:22:39 +0300

    15-Calculating area

---
+++ Problem 15 Calculating Area.cpp
@@ -0,0 +1,18 @@
+
+
+
+//max of 2 number
+#include <iostream>
+
+using namespace std;
+int area(int a,int b)
+{
+      return a*b;
+}
+int main()
+{
+
+    cout<<"The area is "<<area(15,2);
+    return 0;
+}
+
