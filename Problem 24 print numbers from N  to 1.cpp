41d311359278f6e3cf7243aada5dbcd646703519
abdalrahmanDifallh <abddf98@gmail.com>
2025-01-22 23:02:21 +0300

    24-print numbers from N to 1

---
+++ Problem 24 print numbers from N  to 1.cpp
@@ -0,0 +1,17 @@
+
+
+//print numbers from 1 to N
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+  cout<<"Enter N : ";
+  cin>>n;
+  for(int i=n ;i>=0;i--)
+     cout<<i<<" ";
+    return 0;
+}
+
