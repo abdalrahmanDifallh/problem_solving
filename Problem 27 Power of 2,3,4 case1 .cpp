35c088c20cf02d886200cb61421b1b3b8b6f40f6
abdalrahmanDifallh <abddf98@gmail.com>
2025-01-23 23:18:44 +0300

    27-Power 2,3,4

---
+++ Problem 27 Power of 2,3,4 case1 .cpp
@@ -0,0 +1,22 @@
+
+
+
+
+//Power of 2.3.4
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+   cout<<"Enter number for Power ";
+   cin>>n;
+   cout<<"Power ^2 is : "<<n*n<<endl;
+     cout<<"Power ^3 is : "<<n*n*n<<endl;
+       cout<<"Power ^4 is : "<<n*n*n*n<<endl;
+
+
+    return 0;
+}
+
