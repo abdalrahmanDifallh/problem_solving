5b85d27dc1bae3d03c4f23f8552f191b7365f699
abdalrahmanDifallh <abddf98@gmail.com>
2025-01-25 18:46:17 +0300

    37

---
+++ Problem 37 Seconds_to_Days_Hours_Minutes_Seconds.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+
+{
+
+
+   int day,hours,minutes,seconds,sumOfSeconds=0;
+   int d,h1,h2,m1,m2,s1,s2;
+   cout<<"Enter number for Seconds "<<endl;
+   cin>>seconds;
+
+   sumOfSeconds=seconds+(minutes*60)+(hours*3600)+(day*86400);
+  // cout<<seconds/86400<<":"<<seconds/3600<<":"<<seconds/60<<":"<<seconds;
+   d=seconds/84600;
+
+   h1=seconds%84600;
+   h2=h1/3600;
+
+   m1 = h1%3600;
+   m2 = m1/60;
+
+   s1=m1%60;
+
+
+
+
+   cout<<d<<":"<<h2<<":"<<m2<<":"<<s1;
+
+
+    return 0;
+}
+
