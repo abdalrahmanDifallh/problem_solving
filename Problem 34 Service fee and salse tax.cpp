141d78b90b16f31706d44cbb90ca0a311c169bbc
abdalrahmanDifallh <abddf98@gmail.com>
2025-01-25 12:52:38 +0300

    34

---
+++ Problem 34 Service fee and salse tax.cpp
@@ -0,0 +1,25 @@
+
+
+
+
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int billValue,total;
+    float 10totalBill;
+    cout<<"Enter the Bill value : ";
+    cin>>billValue;
+    total = billValue * 1.1;
+    totalBill=total*1.16;
+    cout<<"Total Bill is : "<<totalBill;
+
+
+    return 0;
+}
+
+
