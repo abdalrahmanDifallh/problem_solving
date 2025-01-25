8893c9e301bc0c5ff7c6e06b3bdb6eaa8f68fbe9
abdalrahmanDifallh <abddf98@gmail.com>
2025-01-25 13:40:46 +0300

    35

---
+++ Problem 35 Week and Days.cpp
@@ -0,0 +1,28 @@
+
+
+
+
+
+//is prime number
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+
+{
+
+
+    float numberOfHours,week,day;
+    cout<<"Enter the number of hours : ";
+     cin>>numberOfHours;
+    week = numberOfHours /168;
+    day = numberOfHours / 24;
+    cout<<week.2f<<" Weeks "<<endl;
+    cout<<day<<" Days "<<endl;
+
+    return 0;
+}
+
+
